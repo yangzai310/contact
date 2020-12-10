2d09d3fe4522ec5bfa651fbe857b0e018ffbe206
wangchunyang <yangzai310@163.com>
2020-12-10 18:40:34 +0800

    update

---
+++ src/test/java/com/edu/contact/ContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
