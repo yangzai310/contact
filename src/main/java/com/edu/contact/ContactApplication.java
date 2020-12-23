47fcc5aacf364a9ad3cddd9c6b85bebfe0e67349
wangchunyang <yangzai310@163.com>
2020-12-23 14:24:28 +0800

    lock

--- src/main/java/com/edu/contact/ContactApplication.java
+++ src/main/java/com/edu/contact/ContactApplication.java
@@ -3,11 +3,13 @@ package com.edu.contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.edu.contact")
 public class ContactApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContactApplication.class, args);
     }
+
+
 
 }
---
+++ src/main/java/com/edu/contact/LockController.java
@@ -0,0 +1,33 @@
+package com.edu.contact;
+
+import com.edu.contact.lock.UnSyncToSync;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/lock")
+public class LockController {
+
+    @Resource
+    private UnSyncToSync unSyncToSync;
+
+    @RequestMapping("/lock1")
+    public void doSth() {
+        new Thread(() -> {
+            System.out.println("lock1 sleep before");
+            try {
+                TimeUnit.SECONDS.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("lock1 sleep after");
+            unSyncToSync.done();
+        }).start();
+        unSyncToSync.doVoid();
+        System.out.println("lock1 done -----");
+
+    }
+}
