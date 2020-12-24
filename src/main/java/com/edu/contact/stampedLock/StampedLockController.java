10cc42a599a2bb6571ad1a23c74ca3203d7545be
wangchunyang <yangzai310@163.com>
2020-12-24 10:31:36 +0800

    update

--- src/main/java/com/edu/contact/stampedLock/StampedLockController.java
+++ src/main/java/com/edu/contact/stampedLock/StampedLockController.java
@@ -91,4 +91,37 @@ public class StampedLockController {
             }
         });
     }
+
+
+
+//    final StampedLock sl =
+//            new StampedLock();
+//
+//    // 乐观读
+//    long stamp =
+//            sl.tryOptimisticRead();
+//// 读入方法局部变量
+//......
+//// 校验stamp
+//        if (!sl.validate(stamp)){
+//        // 升级为悲观读锁
+//        stamp = sl.readLock();
+//        try {
+//            // 读入方法局部变量
+//    .....
+//        } finally {
+//            //释放悲观读锁
+//            sl.unlockRead(stamp);
+//        }
+//    }
+////使用方法局部变量执行业务操作
+//......
+//
+//    long stamp = sl.writeLock();
+//try {
+//        // 写共享变量
+//  ......
+//    } finally {
+//        sl.unlockWrite(stamp);
+//    }
 }
