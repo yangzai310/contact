b810d7d45bcabc3c6727e67f33e8ab1e9d9c4a0b
wangchunyang <yangzai310@163.com>
2020-12-31 15:38:00 +0800

    threadpool

---
+++ src/main/java/com/edu/contact/threadpool/MyThreadPoolController.java
@@ -0,0 +1,30 @@
+package com.edu.contact.threadpool;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/threadPool")
+public class MyThreadPoolController {
+
+    private static final MyThreadPool myThreadPool = new MyThreadPool(5, new LinkedBlockingQueue(5));
+
+    @RequestMapping("/test")
+    public void doSth() throws Exception{
+
+        for (int i = 0; i <6; i++) {
+            myThreadPool.execute(() -> {
+                System.out.println("threadName" + Thread.currentThread().getName() + "  start");
+                try {
+                    TimeUnit.SECONDS.sleep(15);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("threadName" + Thread.currentThread().getName() + "  end");
+            });
+        }
+    }
+}
