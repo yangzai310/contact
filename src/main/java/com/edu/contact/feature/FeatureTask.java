845aafc6100752d35a5ab7c3a9c45ea946feec9b
wangchunyang <yangzai310@163.com>
2021-01-07 11:23:31 +0800

    update

--- src/main/java/com/edu/contact/feature/FeatureTask.java
+++ src/main/java/com/edu/contact/feature/FeatureTask.java
@@ -1,23 +1,28 @@
 package com.edu.contact.feature;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class FeatureTask {
 
+    public static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(6);
+
     public static void main(String[] args) {
 
+
         FutureTask<Integer> futureTask = new FutureTask<Integer>(() -> {
+            TimeUnit.SECONDS.sleep(15);
             return 10;
         });
         Callable<Integer> callable = () -> {
+            TimeUnit.SECONDS.sleep(15);
             return 10;
         };
-
-
+//        Future<?> submit = EXECUTOR_SERVICE.submit(futureTask);
+        Future<Integer> submit = EXECUTOR_SERVICE.submit(callable);
         try {
-            futureTask.get();
+//            futureTask.get();
+            submit.get();
+            System.out.println("done ---");
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
