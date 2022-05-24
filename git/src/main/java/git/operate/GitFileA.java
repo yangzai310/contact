87560d16b7d76bbec7abd7223d41701c77054442
wangchunyang <wangchunyang@qijiayoudao.com>
2022-05-25 03:03:32 +0800

    小明第三次需求开发 继续开发 并且完成

--- git/src/main/java/git/operate/GitFileA.java
+++ git/src/main/java/git/operate/GitFileA.java
@@ -2,6 +2,6 @@ package git.operate;
 
 public class GitFileA {
     public static void main(String[] args) {
-        System.out.println("java --- test");
+        System.out.println("【工作区】小明的第三次需求开发 ---");
     }
 }
--- git/src/main/java/git/operate/GitFileB.java
+++ git/src/main/java/git/operate/GitFileB.java
@@ -1,4 +1,7 @@
 package git.operate;
 
 public class GitFileB {
+    public static void main(String[] args) {
+        System.out.println("【暂存区】小明的第三次需求开发 ---");
+    }
 }
