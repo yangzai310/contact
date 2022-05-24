ad1edb34ff84cd774fc13920365002fb22983dcc
wangchunyang <wangchunyang@qijiayoudao.com>
2022-05-25 01:59:12 +0800

    小明第二次需求迭代 出现故障了

--- git/src/main/java/git/operate/GitFileA.java
+++ git/src/main/java/git/operate/GitFileA.java
@@ -2,6 +2,6 @@ package git.operate;
 
 public class GitFileA {
     public static void main(String[] args) {
-        System.out.println("java --- test");
+        System.out.println("java --- test 上线出现故障了");
     }
 }
