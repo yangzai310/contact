889a8a99d82bdd6c4b31733fb799975a5874eed5
wangchunyang <wangchunyang@qijiayoudao.com>
2022-05-25 02:52:15 +0800

    update

--- git/src/main/java/git/operate/GitFileA.java
+++ git/src/main/java/git/operate/GitFileA.java
@@ -2,6 +2,14 @@ package git.operate;
 
 public class GitFileA {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("【工作区】小明的第三次需求开发 ---");
+=======
+        System.out.println("java --- test");
+
+
+        System.out.println("小明的第四次需求开发");
+        System.out.println("小明的第四次需求开发-此行代码是第三次开发需要的代码");
+>>>>>>> 0f3a7fd (小明å第四次需求开发)
     }
 }
--- git/src/main/java/git/operate/GitFileB.java
+++ git/src/main/java/git/operate/GitFileB.java
@@ -1,7 +1,12 @@
 package git.operate;
 
 public class GitFileB {
+<<<<<<< HEAD
     public static void main(String[] args) {
         System.out.println("【暂存区】小明的第三次需求开发 ---");
     }
+=======
+
+
+>>>>>>> 0f3a7fd (小明å第四次需求开发)
 }
