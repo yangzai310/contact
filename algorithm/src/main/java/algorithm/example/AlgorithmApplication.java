76996ee22de16e8a7113ba3b5eb87d25fce097fa
wangchunyang <wangchunyang@qijiayoudao.com>
2023-05-06 15:49:50 +0800

    update

--- algorithm/src/main/java/algorithm/example/AlgorithmApplication.java
+++ algorithm/src/main/java/algorithm/example/AlgorithmApplication.java
@@ -3,11 +3,16 @@ package algorithm.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+import java.util.Stack;
+
 @SpringBootApplication(scanBasePackages = "algorithm.example")
 public class AlgorithmApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlgorithmApplication.class, args);
+//        SpringApplication.run(AlgorithmApplication.class, args);
+
+
     }
 
 }
--- performance/src/main/java/performance/example/PerformanceApplication.java
+++ performance/src/main/java/performance/example/PerformanceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PerformanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PerformanceApplication.class, args);
+        // SpringApplication.run(PerformanceApplication.class, args);
+        // base_size + i * date_type_size
     }
 
 }
